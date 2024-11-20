a3ac18feddcaccdc3aea6ca70e1c1d0423aa6418
SShrey1 <palishrey@gmail.com>
2024-11-20 18:18:31 +0530

    initial 2

--- OrderApp/ResponseModels.swift
+++ OrderApp/ResponseModels.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct MenuResponse : Codable {
+    let items : [MenuItem]
+}
+
+struct CategoriesResponse : Codable {
+    let categories : [String]
+}
+
+struct OrderResponse : Codable{
+    let prepTime : Int
+    
+    enum CodingKeys: String, CodingKey {
+        case prepTime = "prepation_time"
+    }
+}
+
+
+struct Order : Codable{
+    var menuItems : [MenuItem]
+    
+    init(menuItems: [MenuItem] = []) {
+        self.menuItems = menuItems
+    }
+}
